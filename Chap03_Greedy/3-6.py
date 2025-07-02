0b506638ebeeb40206add3bb776b111d98b9318f
SohyeonKim-dev <happythgus89@gmail.com>
2025-07-02 09:23:57 +0900

    [FEAT] 3-6 Solve

--- Chap03_Greedy/3-6.py
+++ Chap03_Greedy/3-6.py
@@ -6,6 +6,11 @@
 S = list(map(int, input()))
 count = 0
 
+diff = 0                    # 변동하는 point 찾기
 
+for i in range(len(S)-1):
+    if S[i] != S[i+1]:
+        diff +=1 
 
-print(count)
+count = (diff+1) // 2       # 변동점 1개 -> 1, 2개 -> 1, 3개 -> 2, 4개 -> 2, 5개 -> 3 ... 
+print(count)                # 따라서 1을 더해준 상태에서, 2로 나눈 몫이 답 (Cycle처럼 그림 그리기)
