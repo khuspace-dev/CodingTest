24bf6703153529f7ef66c3565d97a74c8ec3f109
SohyeonKim-dev <happythgus89@gmail.com>
2025-07-10 16:46:09 +0900

    [FEAT] 4-6 Solve

--- Chap04_Implement/4-10.py
+++ Chap04_Implement/4-10.py
@@ -2,3 +2,8 @@
 # chap4. 335 page
 # https://school.programmers.co.kr/learn/courses/30/lessons/60062
 
+# 외벽의 총 둘레 n 미터
+# 정북 지점 0, 시계방향으로 거리 
+# weak = 취약 지점 배열 
+# dist = 1시간 동안 친구들이 이동 가능한 거리 (시계 or 반시계)
+
--- Chap04_Implement/4-6.py
+++ Chap04_Implement/4-6.py
@@ -8,30 +8,46 @@
 # 0은 홈, 1은 돌기 
 # lock은 고정, key를 움직여, lock's 0을 채우는지 check 
 
-import numpy as np
-
-# 이동 방향 (동, 남, 서, 북)
-dx = [1, 0, -1, 0]
-dy = [0, -1, 0, 1]
-
-M = len(key)
-N = len(lock)
-
 def rotate(key):
     return [list(row) for row in zip(*key[::-1])]
 
 def solution(key, lock):
     answer = False
+
+    M = len(key)
+    N = len(lock)
+
+    new_map = [[1] * (2 * N + M) for _ in range(2 * N + M)]
     
-    while(True):
-        for i in range(4):
-            key = rotate(key)
+    for y in range(N):
+        for x in range(N):
+            new_map[y + M][x + M] = lock[y][x]
 
-            # move 하는 로직 추가해야됨
+    for _ in range(4):
+        key = rotate(key) # 회전된 키 
 
-            if key + lock == np.ones((N, N)):
-                answer = True
-                break
-            else:
-                continue
+        # 키를 움직이는 것
+        for i in range(1, N + M):
+            for j in range(1, N + M):
+
+                # 특정 위치의 키를 더해주는 것 
+                for y in range(M):
+                    for x in range(M):                
+                        new_map[i + y][j + x] = new_map[i + y][j + x] + key[y][x]
+                
+                flag = True
+
+                for y in range(N):
+                    for x in range(N):
+                        if new_map[M+y][M+x] != 1:
+                            flag = False
+                
+                if flag == True:
+                    answer = True 
+                
+                # 더해진 key를 다시 빼주어야 한다. 
+                for y in range(M):
+                    for x in range(M):                
+                        new_map[i + y][j + x] = new_map[i + y][j + x] - key[y][x]
+
     return answer
