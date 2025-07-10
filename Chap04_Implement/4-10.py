9842201c94bdb3ab1b42b828f2cf712451e868f9
SohyeonKim-dev <happythgus89@gmail.com>
2025-07-10 17:00:16 +0900

    [FEAT] 4-10 Solving

--- Chap04_Implement/4-10.py
+++ Chap04_Implement/4-10.py
@@ -7,3 +7,19 @@
 # weak = 취약 지점 배열 
 # dist = 1시간 동안 친구들이 이동 가능한 거리 (시계 or 반시계)
 
+def solution(n, weak, dist):
+    answer = 0
+    
+    diff = []
+    weak.append(weak[1] + n) # 맨 끝에 12 더한 값 추가 
+    
+    for i in range(len(weak) - 1):
+        diff.append(abs(weak[i] - weak[i + 1]))
+    
+    if max(diff) > max(dist):
+        return -1
+    
+    # now what?
+    # 친구들 붙이기 (최소 친구 구하기)
+        
+    return answer
