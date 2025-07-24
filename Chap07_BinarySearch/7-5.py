d4e0dbcf8b5dd48149f5f7d71ab241dfa25127f4
SohyeonKim-dev <happythgus89@gmail.com>
2025-07-24 17:03:41 +0900

    [FEAT] 7-5 Solving

--- Chap07_BinarySearch/7-5.py
+++ Chap07_BinarySearch/7-5.py
@@ -1,2 +1,20 @@
 # 공유기 설치
-# chap7. 368 page
+# chap7. 369 page
+# https://www.acmicpc.net/problem/2110
+
+# 집 N개, 공유기 C개 
+# 가장 인접한 공유기 사이의 거리를 최대
+# 최적화 -> 파라메트릭 서치 (202 page)
+
+N, C = map(int, input().split())
+houses = [int(input()) for _ in range(N)]
+houses.sort()
+
+max_gap = houses[-1] - houses[0]
+
+# 평균처럼 
+# def bs(nums, routers, start, end):
+#     mid = (start + end) // 2
+#     while(start <= end):
+#         if nums[mid] 
+#     return 
---
+++ Chap07_BinarySearch/7-6.py
@@ -0,0 +1,4 @@
+# 가사 검색
+# chap7. 370 page
+# https://school.programmers.co.kr/learn/courses/30/lessons/60060
+
