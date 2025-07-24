05bec89479dd624337f9dec7d0e44f2f3becdc52
SohyeonKim-dev <happythgus89@gmail.com>
2025-07-24 17:12:49 +0900

    [FEAT] 7-6 Solving

--- Chap07_BinarySearch/7-6.py
+++ Chap07_BinarySearch/7-6.py
@@ -2,3 +2,23 @@
 # chap7. 370 page
 # https://school.programmers.co.kr/learn/courses/30/lessons/60060
 
+def binarySearch(words, target, start, end):
+    while(start <= end):
+        mid = (start + end) // 2
+        if words[mid] == target:
+            return mid 
+        elif words[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+def solution(words, queries):
+    words.sort()
+    length = len(words)
+    answer = []
+    
+    for query in queries:
+        binarySearch(words, query, 0, length)
+    
+    return answer
