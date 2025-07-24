57dcace2e698f76f8d8564ad582db76a324d8d71
SohyeonKim-dev <happythgus89@gmail.com>
2025-07-24 18:15:10 +0900

    [FEAT] 8-2 Solve

--- Chap08_DP/8-2.py
+++ Chap08_DP/8-2.py
@@ -7,5 +7,16 @@
 N = int(input())
 foods = list(map(int, input().split()))
 
-dp = [0] * (N)
+dp = [0] * (N + 1)
 
+# 초기화 주의 
+dp[0] = foods[0]
+dp[1] = max(foods[0], foods[1]) # 둘 중 하나를 털면 됨 
+
+# 이후 an 점화식 
+for i in range(2, N):
+    dp[i] = max(dp[i-2] + foods[i], dp[i-1])
+
+# dp[i]는 i번째 창고까지의 최대 식량
+# index 처리로 -1 하는 것 (칸은 4칸 -> 0 ~ 3 매핑)
+print(dp[N - 1])
